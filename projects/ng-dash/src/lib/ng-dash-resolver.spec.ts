8e43d4a776f454e7fc84a1132a1e360608c73d23
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-04-21 16:55:14 +0600

    add tests

--- projects/ng-dash/src/lib/ng-dash-resolver.spec.ts
+++ projects/ng-dash/src/lib/ng-dash-resolver.spec.ts
@@ -1,8 +1,27 @@
 import { NgDashResolver } from "./ng-dash-resolver";
+import { FourColumnLayoutComponent } from './test-doubles/four-column-layout/four-column-layout.component';
 
 describe('NgDashResolver', () => {
-  it('layout should be created', () => {
+  it('dashboard should be created', () => {
     const resolver = new NgDashResolver();
+    let dashboard = resolver.createDashboard({
+      config: {},
+      layoutType: "fourcol",
+      widgets: []
+    });
+
+    expect(dashboard).toBeDefined();
+  });
+
+  it('should create leyout type', () => {
+    const resolver = new NgDashResolver();
+    let dashboard = resolver.bindLayout("fourcol", FourColumnLayoutComponent).createDashboard({
+      config: {},
+      layoutType: "fourcol",
+      widgets: []
+    });
+
+    expect(dashboard.layout).toBe(FourColumnLayoutComponent);
   });
 });
 
